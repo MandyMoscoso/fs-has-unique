c527be31184fac35a3864b3ba0cc0965fb94f6c2
Mandy <quyenmoscoso@gmail.com>
2022-08-03 08:59:09 -0500

    Done

---
+++ hasUniqueChars.js
@@ -0,0 +1,17 @@
+// Write your code below
+
+const hasUniqueChars = (word)=>{
+    answer = true;
+    for (i = 0; i < word.length && answer; i++){
+        for (x =i+ 1; x < word.length && answer; x ++){
+            if (word[i]=== word[x]){
+                return false
+                answer = false;
+            } 
+        }
+        if(i === word.length-1){
+            return true;
+        }
+    }
+}
+console.log(hasUniqueChars("heIiloi"))
